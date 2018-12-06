f5486e3bddaaf2fc8a35f9dcbaee8d17d7ff4fb6
Treasure <treasure.rose@gmail.com>
2018-12-05 16:39:10 -0800

    basic examples complete

--- app.js
+++ app.js
@@ -9,23 +9,23 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static('public'))
 
 // CALL BACKS
-function getUsers(cb){
-  fs.readFile('data.json', 'utf8', (err, data) => {
-    if (err) return cb(err);
-    const result = JSON.parse(data);
-    return cb(result);
-  });
-}
+// function getUsers(cb){
+//   fs.readFile('data.json', 'utf8', (err, data) => {
+//     if (err) return cb(err);
+//     const users = JSON.parse(data);
+//     return cb(users);
+//   });
+// }
 
- app.get('/', (req, res) => {
-  getUsers((result) => {
-    if(!result.users){
-      res.render('error', {error: result});
-    } else {
-      res.render('index', {title: "Profile Page", users: result.users});
-    }
-  });
- });
+//  app.get('/', (req, res) => {
+//   getUsers((result) => {
+//     if(!result.users){
+//       res.render('error', {error: result});
+//     } else {
+//       res.render('index', {title: "Profile Page", users: result.users});
+//     }
+//   });
+//  });
 
 // PROMISES 
 // function getUsers(){
@@ -47,8 +47,32 @@ function getUsers(cb){
 //       res.render('index', {title: "Profile Page", users: users.users});
 //     })
 //     .catch((err) => {
-//       res.render('error', {error: err})
+//       res.render('error', {error: err});
 //     });
 // });
+
+// ASYNC/AWAIT 
+function getUsers(){
+  return new Promise((resolve, reject) => {
+    fs.readFile('data.json', 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        const users = JSON.parse(data);
+        resolve(users);
+      }
+    });
+  });
+}
+
+app.get('/', async (req, res) => {
+  try {
+    const users = await getUsers();
+    res.render('index', {title: "Profile Page", users: users.users});
+  } catch(err){
+    res.render('error', {error: err})
+  }
+});
+
 
 app.listen(3000, () => console.log('App listening on port 3000!'));
