f4f3d6a3284c909b0e6ff7b333c625aa212a1a85
Treasure <treasure.rose@gmail.com>
2018-12-08 21:46:51 -0800

    update code post feedback

--- app.js
+++ app.js
@@ -9,23 +9,23 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static('public'))
 
 // CALL BACKS
-// function getUsers(cb){
-//   fs.readFile('data.json', 'utf8', (err, data) => {
-//     if (err) return cb(err);
-//     const users = JSON.parse(data);
-//     return cb(users);
-//   });
-// }
+function getUsers(cb){
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) return cb(err);
+    const users = JSON.parse(data);
+    return cb(null, users);
+  });
+}
 
-//  app.get('/', (req, res) => {
-//   getUsers((result) => {
-//     if(!result.users){
-//       res.render('error', {error: result});
-//     } else {
-//       res.render('index', {title: "Profile Page", users: result.users});
-//     }
-//   });
-//  });
+app.get('/', (req, res) => {
+  getUsers((err, data) => {
+    if(err){
+      res.render('error', {error: err});
+    } else {
+      res.render('index', {title: "Profile Page", users: data.users});
+    }
+  });
+});
 
 // PROMISES 
 // function getUsers(){
@@ -52,27 +52,27 @@ app.use(express.static('public'))
 // });
 
 // ASYNC/AWAIT 
-function getUsers(){
-  return new Promise((resolve, reject) => {
-    fs.readFile('data.json', 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        const users = JSON.parse(data);
-        resolve(users);
-      }
-    });
-  });
-}
+// function getUsers(){
+//   return new Promise((resolve, reject) => {
+//     fs.readFile('data.json', 'utf8', (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         const users = JSON.parse(data);
+//         resolve(users);
+//       }
+//     });
+//   });
+// }
 
-app.get('/', async (req, res) => {
-  try {
-    const users = await getUsers();
-    res.render('index', {title: "Profile Page", users: users.users});
-  } catch(err){
-    res.render('error', {error: err})
-  }
-});
+// app.get('/', async (req, res) => {
+//   try {
+//     const users = await getUsers();
+//     res.render('index', {title: "Profile Page", users: users.users});
+//   } catch(err){
+//     res.render('error', {error: err})
+//   }
+// });
 
 
 app.listen(3000, () => console.log('App listening on port 3000!'));
